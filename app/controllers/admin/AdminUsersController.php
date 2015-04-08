4bf59e867dd27cc37ae647f17628654d48171edc
Adeel A <adeelbaba@gmail.com>
2015-04-08 16:04:50 -0400

    Rejection - test isRejected

--- app/controllers/admin/AdminUsersController.php
+++ app/controllers/admin/AdminUsersController.php
@@ -313,14 +313,7 @@ class AdminUsersController extends AdminController
                                 @if($username == \'admin\')
                                 @else
                                     <a href="{{{ URL::to(\'admin/users/\' . $id . \'/delete\' ) }}}" class="iframe btn btn-xs btn-danger">{{{ Lang::get(\'button.delete\') }}}</a>
-                                @endif
-                                @if($confirmed==0)
-                                <a href="{{{ URL::to(\'admin/users/\' . $id . \'/activate\' ) }}}"class="iframe btn btn-xs btn-info">{{{ Lang::get(\'Activate\') }}}</a>
-                                <a href="{{{ URL::to(\'admin/users/\' . $id . \'/reject\' ) }}}"class="iframe btn btn-xs btn-info">{{{ Lang::get(\'Reject\') }}}</a>
-                                @elseif($confirmed==2)
-                                <a href="{{{ URL::to(\'admin/users/\' . $id . \'/activate\' ) }}}"class="iframe btn btn-xs btn-info">{{{ Lang::get(\'Activate\') }}}</a>
-                                 @endif
-            ')
+                                @endif')
             ->remove_column('id')
             ->make();
     }
