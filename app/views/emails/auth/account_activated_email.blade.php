24b557f0542d234ad6a94758fecaf21d13d42eac
Adeel A <adeelbaba@gmail.com>
2015-04-08 23:23:12 -0400

    Email Message Changes

--- app/views/emails/auth/account_activated_email.blade.php
+++ app/views/emails/auth/account_activated_email.blade.php
@@ -1,6 +1,3 @@
-<h1>
-    OpenPayments Analytics Account Activated!<br>
-</h1>
 <p>
 <br>
 Hello! {{$name}};<br>
@@ -15,6 +12,7 @@ For technical assistance, please reach out to support@openpaymentsanalytics.com<
 <br>
 Sincerely,<br>
 Open Payments Analytics Team<br>
+
 About Streebo: <br>
 <i>Streebo software and consulting services is<br>a global solutions provider with customers<br>in the US, EMEA, and EU, and has been <br>identified by Inc.500 as one of the<br>fastest growing enterprises three years in a row.</i><br> 
 Website: http://www.streebo.com <br>
@@ -25,6 +23,5 @@ Website: http://www.streebo.com <br>
 <br>
 <small>
 To ensure delivery of Open Payment Analytics emails to your inbox, please add account@openpaymentsanalytics.com to your Email Address Book or Safe List. <br>
-<br>
 You received this email at  because you registered for Open Payments Analytics.<br>
 </small>
--- app/views/emails/auth/account_rejection_email.blade.php
+++ app/views/emails/auth/account_rejection_email.blade.php
@@ -1,12 +1,8 @@
-<h1>
-    OpenPayments Analytics Account Rejected<br>
-</h1>
 <p>
 <br>
-Hello! {{$name}};<br>
-	<br>
+Hello! {{$name}};<br><br>
 <br>
-Thank you for registering at Open Payment Analytics – a one-of-its kind data analytics tool powered by Streebo. <br>
+Thank you for registering at Open Payment Analytics. <br>
 At this time, unfortunately, we are only accepting registrations of individuals within the Life Sciences industry. Our team hopes to reconnect with you as our Open Payments Analytics tool expands to reach a wider audience. <br>
 <br>
 If you are a member of the Pharma industry, and your registration failed to go through, please try registering with a corporate email ID. We apologize for any inconvenience caused. <br>
@@ -16,6 +12,7 @@ For any other questions or comments, feel free to reach out to us at info@openpa
 <br>
 Sincerely,<br>
 Open Payments Analytics Team<br>
+
 About Streebo: <br>
 <i>Streebo software and consulting services is<br>a global solutions provider with customers<br>in the US, EMEA, and EU, and has been <br>identified by Inc.500 as one of the<br>fastest growing enterprises three years in a row.</i><br> 
 Website: http://www.streebo.com <br>
@@ -26,6 +23,5 @@ Website: http://www.streebo.com <br>
 <br>
 <small>
 To ensure delivery of Open Payment Analytics emails to your inbox, please add account@openpaymentsanalytics.com to your Email Address Book or Safe List. <br>
-<br>
 You received this email at  because you registered for Open Payments Analytics.<br>
 </small>
